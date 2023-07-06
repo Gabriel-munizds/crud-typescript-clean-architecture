3f2f137a75d3da34acaa36965eea86a4667bf43f
Gabriel Muniz <gabrielmunizdasilva40@gmail.com>
2023-07-06 11:37:33 -0300

    fix: configurando CORS para receber requisições da aplicação front

--- src/app.ts
+++ src/app.ts
@@ -4,15 +4,19 @@ import { UserController } from './external/controllers/user-controller';
 import { UserService } from './application/user-service';
 import { UserRepositoryImpl } from './external/implementation/user-repository-impl';
 
+
+
 const app: Express = express();
+const cors = require('cors');
 const userRepository = new UserRepositoryImpl();
 const userService = new UserService(userRepository);
 const userController = new UserController(userService);
 const routes = createRoutes(userController);
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000, () => {
-console.log('Server is running on port 3000');
+app.listen(3001, () => {
+console.log('Server is running on port 3001');
 });
